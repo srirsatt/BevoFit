5ee7e78ea2df238607740db37a376587b6845ebc
Sriram Sattiraju <sriramsattiraju07@gmail.com>
2025-12-26 17:02:30 -0600

    updated supabase logic -> new publishable key, initial map pins placed

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,17 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+/*
 const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+*/
+const SUPABASE_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: false,
+    }
+})
--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import { supabase } from '../lib/supabase';
@@ -17,15 +17,50 @@ Whitaker Sports Complex
 
 */
 
-type FacliityMarker = {
+type FacilityMarker = {
     id: string,
     name: string,
     lat: number,
     lng: number,
-}
+};
 
 
 export function Map() {
+    const [pins, setPins] = useState<FacilityMarker[]>([]);
+    // empty arr to start
+    const [loading, setLoading] = useState(true);
+
+    // enter pins from supabase to pins array on load, then map them on succesful load
+    useEffect(() => {
+        let isMounted = true;
+        async function loadPins() {
+            setLoading(true);
+
+            const { data, error } = await supabase
+                .from("facilities")
+                .select("id, name, lat, lng")
+                .not("lat", "is", null)
+                .not("lng", "is", null);
+
+            if (error) {
+                console.error("Error loading facilities from supabase", error);
+                if (isMounted) setPins([]); // sets as blank on error
+            } else {
+                if (isMounted) setPins((data ?? []) as FacilityMarker[]);
+            }
+
+            if (isMounted) {
+                setLoading(false); // finish loading sequence
+            }
+        }
+
+        loadPins();
+
+        return () => {
+            isMounted = false;
+        };
+    }, []);
+
     return (
         <View style={styles.container}>
             <MapView 
@@ -36,12 +71,27 @@ export function Map() {
                     latitudeDelta: 0.01624097636879185,
                     longitudeDelta: 0.0104106601780245,
                 }}
+            >
+                {
+                /*
                 onRegionChangeComplete ={(region) => {
                     console.log("Centering", region.latitude, region.longitude);
                     console.log("Zoom deltas", region.latitudeDelta, region.longitudeDelta);
                 }}
-                
-            />
+                    was for debug -> finding good map coords
+                */
+                }
+                {pins.map((f) => (
+                    <Marker 
+                        key={f.id}
+                        coordinate={{ latitude: f.lat, longitude: f.lng}}
+                        title={f.name}
+                        onPress={() => console.log("clicked on ", f.name)}
+                    
+                    />
+                ))}
+      
+            </MapView>
         </View>
     );
 }
