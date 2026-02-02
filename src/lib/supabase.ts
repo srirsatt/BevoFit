a0c879d28669e39c6f1e6a838d6e9aaff0956ee8
Sriram Sattiraju <sriramsattiraju07@gmail.com>
2026-02-01 19:40:10 -0600

    updated privacy policy html

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -9,6 +9,14 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 */
 const SUPABASE_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE;
 
+if (!SUPABASE_URL) {
+    throw new Error('EXPO_PUBLIC_SUPABASE_URL is not defined in the env variables');
+}
+
+if (!SUPABASE_PUBLISHABLE_KEY) {
+    throw new Error('EXPO_PUBLIC_SUPABASE_PUBLISHABLE is not defined in env variables.');
+}
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
     auth: {
         persistSession: true,
